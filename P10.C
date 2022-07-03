a0ec96f7bd9babcb71614646a7c4a4d5a2050151
zerolionsee <hekun6688@163.com>
2022-07-03 21:37:25 +0800

    函数

---
+++ P10.C
@@ -0,0 +1,23 @@
+/* #include <string.h>
+#include <stdio.h>
+int main()
+{
+    char str1[] = "bit";
+    char str2[] = "##########";
+    strcpy(str2, str1);
+    printf("%s\n", str2);
+    return 0;
+} */
+/* #include <string.h>
+#include <stdio.h>
+int main()
+{
+    char str1[] = "hello,world";
+    memset(str1, '*', 4);
+    printf("%s\n", str1);
+    return 0;
+} */
+
+// www.cplusplus.com
+// en/zh.cppreference.com
+
